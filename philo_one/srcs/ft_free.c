59ed21c07a59934324ae0570489bfd8edd164693
Harthann <nca.eyraud@gmail.com>
2020-10-28 10:02:13 +0100

    mutex destroy

--- philo_one/srcs/ft_free.c
+++ philo_one/srcs/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: nieyraud <nieyraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 15:31:27 by nieyraud          #+#    #+#             */
-/*   Updated: 2020/09/30 10:22:53 by nieyraud         ###   ########.fr       */
+/*   Updated: 2020/10/28 10:01:54 by nieyraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	ft_free(t_philo *list)
 {
 	free(list->status);
+	pthread_mutex_destroy(list->mutex_right);
 	free(list->mutex_right);
 	free(g_printing);
 	free(list);
